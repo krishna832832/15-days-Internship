ffe45f11695f07573efba122fc38d5ddfc9b93d1
krishna832832 <84772011+krishna832832@users.noreply.github.com>
2021-05-27 20:18:53 +0530

    Add files via upload

---
+++ display.php
@@ -0,0 +1,14 @@
+<?php
+
+// by using POST method;
+    $first_name =  $_POST['t1']; //accessing value from 1st text box
+	$last_name =  $_POST['t2'];
+    $email =  $_POST['t3'];
+	
+    
+	echo "</br>$first_name";
+	echo "</br>$last_name" ;
+    echo "</br>$email";
+ ?>
+
+
---
+++ program7.php
@@ -0,0 +1,34 @@
+<html>
+<body>
+   <h2> form </h2>
+    <form action="display.php"method="post">
+	  <table>
+	        <tr>   
+			     <td>First Name </td>
+			    <td><input type="text" name="t1" required/></td
+			 </tr>
+            <tr> 
+		         <td>Last Name </td>
+			     <td><input type="text" name="t2" required/></td>
+			 </tr>
+		   <tr>
+		       <td>Date of Birth </td>
+			   <td><input type="date"  value="" required/></td>
+			 </tr>
+		  <tr>
+		      <td>Email </td>
+			  <td><input type="text" name="t3"/></td>
+			</tr>
+		  <tr>
+		      <td><input type="submit"/></td>
+			  <td><input type="reset"/></td> 
+			 </tr>
+		  
+	    </table>
+		
+	</form>
+  <body>
+  <html>
+			  
+	     
+	    
