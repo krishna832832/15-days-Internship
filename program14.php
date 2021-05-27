0e64302b7a8d2c557972f85671dd74e98f63aead
krishna832832 <84772011+krishna832832@users.noreply.github.com>
2021-05-27 19:57:41 +0530

    Add files via upload

---
+++ program14.php
@@ -0,0 +1,27 @@
+<?php
+$marks =89;
+ if ($marks>=70)
+    {
+      $grade = "first division";
+	  }
+	    
+	   else if ($marks>=65)
+	  {
+	      $grade = "second division";
+		  }
+		  
+		  		  else if ($marks>=45)
+		  { 
+		      $grade = "third division";
+              }
+			  else
+            {
+			     $grade ="fail";
+				 
+                }
+				
+				echo"student grade:$grade";
+ ?>
+			
+			
+	 
