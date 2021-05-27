2a918512936c9b0d31136004604f3b6913763a57
krishna832832 <84772011+krishna832832@users.noreply.github.com>
2021-05-27 17:27:34 +0530

    Add files via upload

---
+++ day3.php
@@ -0,0 +1,44 @@
+<html>
+<body>
+<h1> Regsiteration Form </h1>
+    <form> 
+        <table>
+		    <tr>   
+			     <td>First Name </td>
+			    <td><input type="text" name="txt1" required/></td
+			</tr>
+            <tr>
+		         <td>Last Name </td>
+			     <td><input type="text" name="txt2" required/></td>
+			</tr>
+		  <tr>
+		       <td>Date of Birth </td>
+			   <td><input type="date" value="2017-06-02" required/></td>
+			</tr>
+		  <tr>
+		      <td>Email id </td>
+			  <td><input type="email" name="txt3"/></td>
+			</tr>
+		  <tr>
+		      <td><input type="submit"/><td>
+			 </tr>
+		  <tr>
+		      <td><input type="reset"/></td> 
+	    </table>
+		
+	</form>
+  <body>
+  <html>
+			  
+			   
+			
+		
+			
+		
+			  
+			
+			
+			
+              
+			
+			
---
+++ program13.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+
+<?php 
+   echo "<table border='0'>";
+  for($x=0;$x<=200;$x++) {
+	  echo "<tr>";
+	  if($x%2==0) {
+		  echo "<td  bgcolor='light gregoriantojd'>$x</td>";
+	  }else {
+           echo "<td bgcolor='yellow'>$x</td>";
+	  }
+	   echo"</tr>";
+  }
+	   echo "</table>";
+	 ?>
+	 <body>
+	 <html>
+  
