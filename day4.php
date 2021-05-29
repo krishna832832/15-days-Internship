8375ce23d398e21b7a8ed3a2d0fb6553780eef70
krishna832832 <84772011+krishna832832@users.noreply.github.com>
2021-05-29 17:22:05 +0530

    Add files via upload

---
+++ day4.php
@@ -0,0 +1,142 @@
+<?php
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>krishna</title>
+<link href="default.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<div id="menu-wrapper">
+    <div id="menu">
+       <ul>
+		<li class="current_page_item"><a href="#">Homepage</a></li>
+		<li><a href="#">Products</a></li>
+		<li><a href="#">Services</a></li>
+		<li><a href="#">About Us</a></li>
+		<li><a href="#">Contact Us</a></li>
+	  </ul>
+  </div>
+          <!-- end menu -->
+</div>
+<div id="header-wrapper">
+      <div id="header">
+       <div id="logo">
+	                <h1><a href="#">krishna Thakkar</a></h1>
+				</div>
+		</div>
+ </div>			
+<div id="wrapper">
+        <!-- end #header -->
+		<div id="page-bgtop"></div>
+		<div id="page">
+		     <div><img src="img3.jpg" width="920" height="300" alt="" /></div>
+	<div id="content">
+		<div class="post">
+			<h1 class="title"><a href="#">Welcome to Our Website!</a></h1>
+			<p class="byline"><small> <a href="http://www.freecsstemplates.org/"></a></small></p>
+			<div class="entry">
+				<p><strong>Golden Gate Bridge is a suspension bridge across the place the where San Francisco Bay opens into the Pacific Ocean, so-called Golden Gate (hence the name). It connects San Francisco with Marin County and it is 2737 meters long and 227 meters high. Some 45 million of vehicles cross Golden Gate Bridge in a year.</p>
+				
+			</div>
+		</div>
+
+			<h2 class="title"><a href="#">Gallery</a></h2>
+			<div><img src="image_9.jpeg" width="920" height="300" alt="" /></div>
+			<div><img src="image_10.jpeg" width="920" height="300" alt="" /></div>
+		</div>
+	</div>
+	<!-- end content -->
+	<!-- start sidebars -->
+	
+	<div id="sidebar1" class="sidebar">
+			
+				<h2>Calendar</h2>
+				<div id="calendar_wrap">
+					<table width="80%" summary="Calendar">
+						<caption>
+						June 2021
+						</caption>
+						<thead>
+							<tr>
+								<th abbr="Monday" scope="col" title="Monday">M</th>
+								<th abbr="Tuesday" scope="col" title="Tuesday">T</th>
+								<th abbr="Wednesday" scope="col" title="Wednesday">W</th>
+								<th abbr="Thursday" scope="col" title="Thursday">T</th>
+								<th abbr="Friday" scope="col" title="Friday">F</th>
+								<th abbr="Saturday" scope="col" title="Saturday">S</th>
+								<th abbr="Sunday" scope="col" title="Sunday">S</th>
+							</tr>
+						</thead>
+						<tfoot>
+							<tr>
+								<td abbr="June" colspan="3" id="prev"><a href="#" title="View posts for June 2021">&laquo; June</a></td>
+								<td class="pad">&nbsp;</td>
+								<td colspan="3" id="next">&nbsp;</td>
+							</tr>
+						</
+						<tbody>
+							<tr>
+								<td>1 <id="today"></td>
+								<td>2</td>
+								<td>3</td>
+								<td>4</td>
+								<td>5</td>
+								<td>6</td>
+								<td>7</td>
+							</tr>
+							<tr>
+								<td>8</td>
+								<td>9</td>
+								<td>10</td>
+								<td>11</td>
+								<td>12</td>
+								<td>13</td>
+								<td>14</td>
+							</tr>
+							<tr>
+								<td>15</td>
+								<td>16</td>
+								<td>17</td>
+								<td>18</td>
+								<td>19</td>
+								<td>20</td>
+								<td>21</td>
+							</tr>
+							<tr>
+								<td>22</td>
+								<td>23</td>
+								<td>24</td>
+								<td>25</td>
+								<td>26</td>
+								<td>27</td>
+								<td>28</td>
+							</tr>
+							<tr>
+								<td>29</td>
+								<td>30</td>
+								<td>31</td>
+								<td class="pad" colspan="4">&nbsp;</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</li>
+			
+		</ul>
+	</div>
+	<!-- end sidebars -->
+	<div style="clear: both;">&nbsp;</div>
+</div>
+<!-- end page -->
+</div>
+<div id="footer">
+</div>
+</body>
+</html>
+?>
+
